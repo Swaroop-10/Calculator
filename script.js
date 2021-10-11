43799f86901af81e00f1f1af5725b593539c5758
Swaroop-10 <swaroopmhasawade10@gmail.com>
2021-10-11 15:17:34 +0530

    commit_1

--- script.js
+++ script.js
@@ -1,75 +1,27 @@
-function getHistory() {
-  return document.getElementById("history-value").innerText;
+let lightTheme = "styles/light.css";
+let darkTheme = "styles/dark.css";
+
+// Clears the screen on click of C button.
+function clearScreen() {
+  document.getElementById("result").value = "";
 }
-function printHistory(num) {
-  document.getElementById("history-value").innerText = num;
+// Displays entered value on screen.
+function liveScreen(value) {
+  let res = document.getElementById("result");
+  if (res.value == "undefined") {
+    res.value = "";
+  }
+  res.value += value;
 }
-function getOutput() {
-  return document.getElementById("output-value").innerText;
-}
-function printOutput(num) {
-  if (num == "") {
-    document.getElementById("output-value").innerText = num;
+// Swaps the style sheet in order to  achieve dark mode.
+function changeTheme() {
+  let darkMode = document.getElementById("dark-mode");
+  let theme = document.getElementById("theme");
+  if (theme.getAttribute("href") == lightTheme) {
+    theme.href = darkTheme;
+    darkMode.innerHTML = "Light Mode 🌞";
   } else {
-    document.getElementById("output-value").innerText = getFormattedNumber(num);
+    theme.href = lightTheme;
+    darkMode.innerHTML = "Dark Mode 🌙";
   }
-}
-function getFormattedNumber(num) {
-  if (num == "-") {
-    return "";
-  }
-  var n = Number(num);
-  var value = n.toLocaleString("en");
-  return value;
-}
-function reverseNumberFormat(num) {
-  return Number(num.replace(/,/g, ""));
-}
-var operator = document.getElementsByClassName("operator");
-for (var i = 0; i < operator.length; i++) {
-  operator[i].addEventListener("click", function () {
-    if (this.id == "clear") {
-      printHistory("");
-      printOutput("");
-    } else if (this.id == "backspace") {
-      var output = reverseNumberFormat(getOutput()).toString();
-      if (output) {
-        //if output has a value
-        output = output.substr(0, output.length - 1);
-        printOutput(output);
-      }
-    } else {
-      var output = getOutput();
-      var history = getHistory();
-      if (output == "" && history != "") {
-        if (isNaN(history[history.length - 1])) {
-          history = history.substr(0, history.length - 1);
-        }
-      }
-      if (output != "" || history != "") {
-        output = output == "" ? output : reverseNumberFormat(output);
-        history = history + output;
-        if (this.id == "=") {
-          var result = eval(history);
-          printOutput(result);
-          printHistory("");
-        } else {
-          history = history + this.id;
-          printHistory(history);
-          printOutput("");
-        }
-      }
-    }
-  });
-}
-var number = document.getElementsByClassName("number");
-for (var i = 0; i < number.length; i++) {
-  number[i].addEventListener("click", function () {
-    var output = reverseNumberFormat(getOutput());
-    if (output != NaN) {
-      //if output is a number
-      output = output + this.id;
-      printOutput(output);
-    }
-  });
 }
